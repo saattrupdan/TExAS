43baad09c1ee741701700d64ada654a8bd42d747
saattrupdan <saattrupdan@gmail.com>
2022-01-22 21:26:55 +0000

    fix: Return beam_values properly

--- src/answer_extraction.py
+++ src/answer_extraction.py
@@ -150,10 +150,8 @@ def extract_translated_answer(answer_token_idxs: List[int],
             beam_values = beam[0]
             beam_idxs = all_beam_idxs[beam[1]]
 
-        breakpoint()
-
         # We now have the top `beam_width` best combinations of target tokens
         # stored in the `beam_idxs` tensor. We now extract the best combination
         # out of these, and return it.
         best_combination = beam_idxs[beam_values.argmax()]
-        return best_combination.tolist(), beam_values.max()
+        return best_combination.tolist(), beam_values.max().item()
