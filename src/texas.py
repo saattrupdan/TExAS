5948e964fc2aeb59cb02910c1d3b059faaa53b02
saattrupdan <saattrupdan@gmail.com>
2022-01-22 21:10:03 +0000

    fix: Try not zeroing in case 5

--- src/texas.py
+++ src/texas.py
@@ -579,8 +579,8 @@ class Texas:
 
                 # Zero the cross-attention values at tokens outside the
                 # (ts, te) interval
-                cross_attentions[:, :ts, :] = 0.
-                cross_attentions[:, te:, :] = 0.
+                # cross_attentions[:, :ts, :] = 0.
+                # cross_attentions[:, te:, :] = 0.
 
                 # Use the cross attentions to find the location of the
                 # translated answer
