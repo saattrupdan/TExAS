424de607eaeddf6dbab14a02bad728d12b65cb79
saattrupdan <saattrupdan@gmail.com>
2022-01-22 17:02:21 +0000

    fix: Uncomment

--- src/texas.py
+++ src/texas.py
@@ -614,24 +614,24 @@ if __name__ == '__main__':
     texas = Texas()
 
     # SQuAD 2.0
-    # params = dict(dataset_id='squad_v2',
-    #               target_language='da')
-    # for split in ['train', 'validation']:
-    #     texas.translate_dataset(split=split, **params)
+    params = dict(dataset_id='squad_v2',
+                  target_language='da')
+    for split in ['train', 'validation']:
+        texas.translate_dataset(split=split, **params)
 
     # Adversarial QA
-    # params = dict(dataset_id='adversarial_qa',
-    #               dataset_subset_id='adversarialQA',
-    #               target_language='da')
-    # for split in ['train', 'validation', 'test']:
-    #     texas.translate_dataset(split=split, **params)
+    params = dict(dataset_id='adversarial_qa',
+                  dataset_subset_id='adversarialQA',
+                  target_language='da')
+    for split in ['train', 'validation', 'test']:
+        texas.translate_dataset(split=split, **params)
 
     # SberQuAD
-    # params = dict(dataset_id='sberquad',
-    #               target_language='da',
-    #               sentence_splitter='ru_core_news_sm')
-    # for split in ['train', 'validation', 'test']:
-    #     texas.translate_dataset(split=split, **params)
+    params = dict(dataset_id='sberquad',
+                  target_language='da',
+                  sentence_splitter='ru_core_news_sm')
+    for split in ['train', 'validation', 'test']:
+        texas.translate_dataset(split=split, **params)
 
     # GermanQuAD
     def answer_idx_fn(example: dict):
