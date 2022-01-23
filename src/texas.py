8b64e272af3a4975baff6050f2e94009eaf481a1
saattrupdan <saattrupdan@gmail.com>
2022-01-23 14:12:21 +0000

    fix: Fixed error with char_s

--- src/texas.py
+++ src/texas.py
@@ -290,10 +290,6 @@ class Texas:
         # Initialise the dataset streaming
         dataset = load_dataset(dataset_id, dataset_subset_id, split=split)
 
-        # TEMP
-        from datasets import Dataset
-        dataset = Dataset.from_dict(dataset[6850:])
-
         # Shortened variables for the translation model
         tokenizer = self.translation_tokenizer
 
