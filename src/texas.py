00210e863ccde0c01f5e40dc0b999fca15d70731
saattrupdan <saattrupdan@gmail.com>
2022-01-23 21:27:36 +0000

    feat: Try zh

--- src/texas.py
+++ src/texas.py
@@ -621,7 +621,7 @@ if __name__ == '__main__':
 
     # SQuAD 2.0
     params = dict(dataset_id='squad_v2',
-                  target_language='fr')
+                  target_language='zh')
     for split in ['train', 'validation']:
         texas.translate_dataset(split=split, **params)
 
