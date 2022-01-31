a7c062b38d1d167daa7ddecbacb2d8e25afe51e5
saattrupdan <saattrupdan@gmail.com>
2022-01-31 20:59:22 +0000

    chore: Remove unused val.py

--- src/val.py
+++
@@ -1,6 +0,0 @@
-'''Validation script for question-answering models.
-
-This is based on the example at:
-    https://colab.research.google.com/github/huggingface/notebooks/blob/master/examples/question_answering.ipynb
-'''
-
