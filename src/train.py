35c60785937b55d108d5954858f070a98c503230
saattrupdan <saattrupdan@gmail.com>
2022-01-31 20:59:31 +0000

    refactor: Separate evaluate function

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ from transformers import (AutoModelForQuestionAnswering,
                           TrainingArguments,
                           default_data_collator,
                           Trainer)
+from typing import Dict
 
 from data_preparation import QAPreparer
 from config import Config
@@ -79,6 +80,31 @@ def train(dataset_dict: DatasetDict, output_model_id: str, config: Config):
     # Initialise the test dataset
     test_dataset = dataset_dict['validation']
 
+    # Evaluate the model
+    scores = evaluate(test_dataset, trainer, preparer)
+
+    # Print the results
+    print(f'EM: {scores["em"]}:.3f}')
+    print(f'F1: {scores["f1"]}:.3f}')
+
+
+def evaluate(test_dataset: Dataset,
+             trainer: Trainer,
+             preparer: QAPreparer) -> Dict[str, float]:
+    '''Evaluate the finetuned model on a test dataset.
+
+    Args:
+        test_dataset (Dataset):
+            The test dataset to evaluate the model on.
+        trainer (Trainer):
+            The trainer object to use to evaluate the model.
+        preparer (QAPreparer):
+            The preparer object to use to prepare the test dataset.
+
+    Returns:
+        Dict[str, float]:
+            A dictionary containing the EM and F1 scores.
+    '''
     # Prepare the test dataset
     prepared_test = preparer.prepare_test_dataset(test_dataset)
 
@@ -95,7 +121,7 @@ def train(dataset_dict: DatasetDict, output_model_id: str, config: Config):
     # Load metric
     metric = load_metric('squad_v2')
 
-    # Compute metric score
+    # Compute metric scores
     predictions = [
         dict(id=k, prediction_text=v, no_answer_probability=0.)
         for k, v in predictions.items()
@@ -107,12 +133,9 @@ def train(dataset_dict: DatasetDict, output_model_id: str, config: Config):
         for example in test_dataset
     ]
     scores = metric.compute(predictions=predictions, references=references)
-    em_score = scores['exact']
-    f1_score = scores['f1']
 
-    # Print the results
-    print(f'EM: {em_score:.3f}')
-    print(f'F1: {f1_score:.3f}')
+    # Return the scores
+    return dict(em=scores['exact'], f1=scores['f1'])
 
 
 if __name__ == "__main__":
@@ -131,9 +154,9 @@ if __name__ == "__main__":
     ))
 
     # Load config
-    config = Config()
+    config = Config(model_id='vesteinn/IceBERT')
 
     # Train the model
     train(dataset_dict,
-          output_model_id=f'saattrupdan/xlmr-base-texas-squad-{language_code}',
+          output_model_id=f'saattrupdan/icebert-texas-squad-{language_code}',
           config=config)
